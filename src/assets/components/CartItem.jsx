202852ff6f2612dddec465cf522de3e4e7d12c92
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-09-29 20:44:54 +0700

    menampilkan total harga dan memberikan styling pada cartItem

--- src/assets/components/CartItem.jsx
+++ src/assets/components/CartItem.jsx
@@ -40,29 +40,29 @@ function CartItem({ onUpdateTotalItems }) {
         onUpdateTotalItems(total); // Update total items di Navbar
     }, [cartItems, onUpdateTotalItems]);
 
-    
     return (
-        <div>
+        <div style={{ backgroundColor: '#FFCFB3', color: '#fff', marginBottom: '10px', borderRadius:'1%'}}>
             {cartItems.length === 0 ? <h1>Loading...</h1> : 
-            <div>
-                <div key={item.id}>
-                    <img src={item.image} alt="" width={80} />
-                    <p>{item.title}</p>
-                    <p>${item.price}</p>
+            <div style={{padding:'2rem', color:'black'}}>{cartItems.map((item) => 
+                <div key={item.id} style={{ display: 'flex', alignItems: 'center', padding: '30px', justifyContent: 'space-between'}}>
+                    <img src={item.image} alt="" width={80} style={{ flexShrink: 0,  border: '3px solid #E78F81', borderRadius:'5%', padding:'5px'}} />
+                    <p style={{ width: '200px',textAlign:'start', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{item.title}</p>
+                    <p style={{ width: '80px', textAlign:'start' }}>${item.price}</p>
 
+                    {/* mengoper item ID dan quantity ke komponen Counter */}
                     <div style={{ width: '120px'}}>
-                        <Counter
-                        itemId={item.id} 
-                        quantity={item.quantity} 
-                        onQuantityChange={handleQuantityChange}
+                        <Counter 
+                            itemId={item.id} 
+                            quantity={item.quantity} 
+                            onQuantityChange={handleQuantityChange} 
                         />
                     </div>
                     
-                    <div>
-                        Total: $
+                    <div style={{ width: '120px', textAlign:'start', borderBottom:'1px solid #E78F81' }}>
+                        Total: ${item.price * item.quantity} {/* Menghitung Harga Akhir */}
                     </div>
                 </div>
-            </div>
+            )}</div>
             }
         </div>
     );
