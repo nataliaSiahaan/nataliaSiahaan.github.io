705aad0d406c2eb3c4ec8e2cf40d78c2e518e10f
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-09-29 20:34:27 +0700

    fetching data API dan menampilkan di return

--- src/assets/components/CartItem.jsx
+++ src/assets/components/CartItem.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Counter from "./Counter";
 
-const CartItem = () => {
-  return (
-    <div>
-      <h3>Item</h3>
-      <p>Quantity: 1</p>
-      <p>Price: $20</p>
-    </div>
-  );
-};
+function CartItem({ onUpdateTotalItems }) {
+    const [cartItems, setCartItems] = useState([]);
+    
+    // Fetching data dari API
+    async function getCartItem() {
+        let URL = "https://fakestoreapi.com/products?limit=5";
+        const response = await fetch(URL);
+        const result = await response.json();
+
+        const itemsWithQuantity = result.map(item => ({
+            ...item,
+            quantity: 1  // Inisialisasi jumlah setiap item menjadi 1
+        }));
+        setCartItems(itemsWithQuantity);
+    }
+
+    useEffect(() => {
+        getCartItem();
+    }, []);
+
+    return (
+        <div>
+            {cartItems.length === 0 ? <h1>Loading...</h1> : 
+            <div>
+                <div key={item.id}>
+                    <img src={item.image} alt="" width={80} />
+                    <p>{item.title}</p>
+                    <p>${item.price}</p>
+
+                    <div style={{ width: '120px'}}>
+                        <Counter/>
+                    </div>
+                    
+                    <div>
+                        Total: $
+                    </div>
+                </div>
+            </div>
+            }
+        </div>
+    );
+}
 
 export default CartItem;
