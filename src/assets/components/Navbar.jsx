459b847aa92d7aea7b575d9384b3bc73ae66b693
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-09-29 20:18:02 +0700

    basic structure komponen Navbar

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,13 @@
+// Navbar.jsx
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>MyStore</h1>
+      <div>Cart</div>
+    </nav>
+  );
+};
+
+export default Navbar;
