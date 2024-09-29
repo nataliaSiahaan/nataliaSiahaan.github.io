9191be13e74e2b3b88ac1aa1b0e1151100fed184
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-09-29 20:20:33 +0700

    props totalItems telah ditambahkan dan pemberian styling

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Navbar = ({ totalItems }) => {
     return(
-        <nav>
+        <nav style={{display:"flex", justifyContent:"space-between", padding:"0 5rem", backgroundColor:"#FFCFB3", borderRadius:"1rem", color:"white", boxShadow:"5px 5px 5px rgba(0,0,0, 0.5)"}}>
             <h2>MyStore</h2>
-            <div>
-                <p>Cart</p>
-                <span>{totalItems}</span>
+            <div style={{ position: 'relative', alignItems:'center', justifyContent:'center', top:'5px', right:'15px'}}>
+                <p style={{ fontSize: '18px' }}>Cart</p>
+                <span style={{ position: 'absolute', top: '-1px', right: '-10px', backgroundColor: '#E78F81', borderRadius: '50%', color: '#fff', padding:'0 5px' }}>{totalItems}</span>
             </div>
         </nav>
     );
