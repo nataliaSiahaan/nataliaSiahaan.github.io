8d31854ab881fc2405c803c587acc5e0f94a33cf
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-09-29 20:27:41 +0700

    struktur dasar CartItem

---
+++ src/assets/components/CartItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CartItem = () => {
+  return (
+    <div>
+      <h3>Item</h3>
+      <p>Quantity: 1</p>
+      <p>Price: $20</p>
+    </div>
+  );
+};
+
+export default CartItem;
